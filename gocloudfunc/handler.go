25c4c8caf5cf4625e2e4356f0b3dadaba556a64e
Martin Dekov <34942004+martindekov@users.noreply.github.com>
2018-10-12 17:38:43 +0300

    Update handler.go

--- gocloudfunc/handler.go
+++ gocloudfunc/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Non master change again, Go. You said: %s", string(req))
+	return fmt.Sprintf("Non master change again , Go. You said: %s", string(req))
 }
